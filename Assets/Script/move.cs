fe736a52a72f2b199065fb80c89d6b4149601749
LilianAragou <114906517+LilianAragou@users.noreply.github.com>
2025-03-31 10:49:50 +0200

    Update

--- Assets/Script/move.cs
+++ Assets/Script/move.cs
@@ -1,33 +1,54 @@
 using System;
 using UnityEngine;
 
-public class move : MonoBehaviour {
-
+public class Move : MonoBehaviour
+{
     public float speed = 5f;
     public float jump = 8f;
-    private Rigidbody2D rb;
+    public float wallJumpForce = 6f;
+    public Transform shadowPrefab;
 
-    private CapsuleCollider2D monColl;
+    private Rigidbody2D rb;
+    public CapsuleCollider2D monColl;
     private Collider2D[] colls;
     private bool grounded;
     private float rayonDetection;
+    private bool wallSliding;
+    private bool touchingWallLeft, touchingWallRight;
     private SpriteRenderer skin;
     private Animator anim;
+    private Transform shadow;
+    private float lastWallJumpTime;
+    public LayerMask groundLayer;
+    public Transform groundChecker;
 
-
-    void Start() {
+    void Start()
+    {
+        
         monColl = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         skin = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
+
+        // Création de l'ombre
+        shadow = Instantiate(shadowPrefab, transform.position, Quaternion.identity);
+        shadow.GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0.5f); // Ombre semi-transparente
     }
 
-    void Update() {
+    void Update()
+    {
         groundCheck();
         moveCheck();
         flipCheck();
         animCheck();
-    }    
+        shadowUpdate();
+
+        // Échange de position avec l'ombre
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SwapWithShadow();
+        }
+    }
 
     void groundCheck() {
         grounded = false;
@@ -49,22 +70,41 @@ public class move : MonoBehaviour {
         }
     }
 
-    private void flipCheck() {
-        if(Input.GetAxis("Horizontal") > 0) {
-            skin.flipX = false;
-        }
 
-        if (Input.GetAxis("Horizontal") < 0) {
-            skin.flipX = true;
+    void flipCheck()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            skin.flipX = Input.GetAxis("Horizontal") < 0;
         }
     }
 
-    void animCheck() {
-        anim.SetFloat("velocityX", Mathf.Abs(rb.linearVelocityX));
-        anim.SetFloat("velocityY", rb.linearVelocityY);
+    void animCheck()
+    {
+        anim.SetFloat("velocityX", Mathf.Abs(rb.linearVelocity.x));
+        anim.SetFloat("velocityY", rb.linearVelocity.y);
         anim.SetBool("grounded", grounded);
     }
 
+    void shadowUpdate()
+    {
+        if (shadow != null)
+        {
+            Vector3 mirroredPosition = new Vector3(-transform.position.x, transform.position.y, transform.position.z);
+            shadow.position = mirroredPosition;
+            shadow.rotation = transform.rotation;
+        }
+    }
+
+    void SwapWithShadow()
+    {
+        if (shadow != null)
+        {
+            Vector3 tempPosition = transform.position;
+            transform.position = shadow.position;
+            shadow.position = tempPosition;
+        }
+    }
     private void OnDrawGizmos() {
         if (monColl == null) {
             monColl = GetComponent<CapsuleCollider2D>();
